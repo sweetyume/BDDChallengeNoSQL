c76f96ec3bfd4913f10d371093d9a152d62dc98c
sweetyume <sweet.tsukiyume@gmail.com>
2018-05-15 15:22:10 +0200

    add comment route

--- routes/ownerRouter.js
+++ routes/ownerRouter.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import bodyParser from 'body-parser';
 
 import Owner from '../model/owner';
+import Comment from '../model/comment';
 
 const router = Router();
 
@@ -15,6 +16,7 @@ router.post('/add', bodyParser.urlencoded({ extended: false }), (req, res) => {
 });
 
 router.get('/', (req, res) => {
+    debugger
     Owner.find((err, owners) => {
         if (err) { res.send(err); }
         res.json(owners);
@@ -41,4 +43,40 @@ router.delete('/:id/delete', (req, res) => {
         res.json({message: `${owner.username} has been removed`});
     });
 });
+
+// comments
+router.post('/:id/comment/create', bodyParser.urlencoded({ extended: true }), (req, res) => {
+    // Création du nouveau commentaire
+    const newComment = new Comment({text: req.body.comments});
+    // sauvegarde du commentaire dans la collection
+    newComment.save((err, comment) => {
+        console.log(comment)
+        if (err) { res.send(err) }
+        // on trouve l'owner en question pour mettre à jour
+        // le tableau de commentaires
+        Owner.findByIdAndUpdate(
+            // id de l'owner en question
+            req.params.id,
+            // MAJ du tableau de commentaire avec id du nouveau commentaire
+            { $push: { comments: comment._id } },
+            (err, comment) => {
+                err ?  res.send(err)
+                    : res.json({ "message": "a comment has been created" })
+
+            })
+    })
+});
+
+// get  all comments for one owner
+
+router.get('/:id/comments', (req, res) => {
+    Owner.findById(req.params.id)
+        .populate('comments') // remplace les id trouvés dans tableau comments par le contenu du doc
+        .exec((err, owner) => {
+            if (err) res.send(err);
+            console.log(owner)
+            res.send(owner.comments);
+        })
+});
+
 export default router;
